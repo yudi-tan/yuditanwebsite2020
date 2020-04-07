1c6882cb16de0ba6ca1f752f3147a24c5678974b
Yudi Tan <yudi98@berkeley.edu>
2020-04-06 19:18:10 -0700

    initial scaffold

---
+++ components/terminal.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Terminal extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>HELLO NERDS</h1>
+      </div>
+    );
+  }
+}
+
+export default Terminal;
